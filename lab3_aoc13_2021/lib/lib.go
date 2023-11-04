986acb55388427c6c0d1c975f068ad7ade76b4d4
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-11-04 22:18:13 +0100

    lab3_aoc, parse input

--- lab3_aoc13_2021/lib/lib.go
+++ lab3_aoc13_2021/lib/lib.go
@@ -1,1 +1,77 @@
 package lib
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type point struct {
+	x, y int
+}
+
+type command struct {
+	op    byte
+	coord int
+}
+
+type TPaper struct {
+	points []point
+	cmd    []command
+}
+
+func InitPaper(r io.Reader) *TPaper {
+	scanner := bufio.NewScanner(r)
+
+	p := []point{}
+	cmd := []command{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		if strings.Contains(line, "fold along") {
+			op := strings.Split(strings.Split(line, " ")[2], "=")
+
+			coord, err := strconv.Atoi(op[1])
+			if err != nil {
+				fmt.Println("Error converting coordinate to int")
+				return nil
+			}
+
+			if op[0] == "x" || op[0] == "y" {
+				cmd = append(cmd, command{op[0][0], coord}) // this is a bit ugly
+			} else {
+				fmt.Println("Error parsing fold command")
+				return nil
+			}
+
+		} else {
+			coords := strings.Split(line, ",")
+
+			x, err := strconv.Atoi(coords[0])
+			if err != nil {
+				fmt.Printf("Error converting x: %s coordinate to int\n", coords[0])
+				return nil
+			}
+
+			y, err := strconv.Atoi(coords[1])
+			if err != nil {
+				fmt.Printf("Error converting y: %s coordinate to int\n", coords[1])
+				return nil
+			}
+			p = append(p, point{x, y})
+		}
+	}
+
+	return &TPaper{p, cmd}
+}
+
+func (p *TPaper) Fold() {
+	fmt.Println("FOLD")
+}
--- lab3_aoc13_2021/main.go
+++ lab3_aoc13_2021/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+	"transparent_origami/lib"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	input := `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`
+
+	paper := lib.InitPaper(strings.NewReader(input))
+	paper.Fold()
 }
