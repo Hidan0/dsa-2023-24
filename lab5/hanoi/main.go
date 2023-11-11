b0054f2cc11a2694bd4be463dc6a43b6cd6b60b4
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-11-11 18:51:37 +0100

    lab5, number of moves hanoi

--- lab5/hanoi/main.go
+++ lab5/hanoi/main.go
@@ -12,6 +12,23 @@ func hanoi(n, from, tmp, to int) {
 	hanoi(n-1, tmp, from, to)
 }
 
+func numberOfMovesHanoi(n, from, tmp, to int) uint64 {
+	if n <= 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	numberOfMovesHanoi(n-1, from, to, tmp)
+	return 2*numberOfMovesHanoi(n-1, tmp, from, to) + 1
+}
+
 func main() {
 	hanoi(3, 0, 1, 2)
+
+	fmt.Printf("To move 3 disks, we need %d moves\n", numberOfMovesHanoi(3, 0, 1, 2))
+	fmt.Printf("To move 10 disks, we need %d moves\n", numberOfMovesHanoi(10, 0, 1, 2))
+	// fmt.Printf("To move 64 disks, we need %d moves\n", numberOfMovesHanoi(64, 0, 1, 2)) // end of the world
 }
